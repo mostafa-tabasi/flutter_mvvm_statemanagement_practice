e890125c1912f5f47684214571a16ad15f986abe
Mostafa Tabasi <dev.mostafatabasi@gmail.com>
2026-01-04 19:26:58 +0330

    removed equatable dependency

--- lib/models/genre.dart
+++ lib/models/genre.dart
@@ -1,13 +1,8 @@
-import 'package:equatable/equatable.dart';
-
-class Genre extends Equatable {
+class Genre {
   final int id;
   final String name;
 
   const Genre({required this.id, required this.name});
-
-  @override
-  List<Object> get props => [id, name];
 
   factory Genre.fromJson(Map<String, dynamic> json) {
     return Genre(id: json['id'], name: json['name']);
